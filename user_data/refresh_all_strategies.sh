a6d85dbd4b83865a94c8c76695a8bb9da853933f
Ciprian <ciprianosss@gmail.com>
2021-07-03 18:36:48 +0300

    Script updates

--- user_data/refresh_all_strategies.sh
+++ user_data/refresh_all_strategies.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
-echo "docker-compose run --rm freqtrade backtesting --datadir user_data/data/binance --export trades --stake-amount 100 -i 15m --timerange=20210101- --strategy-list " > run_all_strats.sh
-ls -1a strategies/*.py > all_strategies.txt
-cat all_strategies.txt | sed 's/.py/\\/' > all_strategies.txt
-#cat all_strategies.txt | sed 's/strategies\///' > all_strategies.txt &&
-#cat all_strategies.txt >> run_all_strats.sh &&
-#echo "done"
+echo "! Creating run_all_strats.sh file"
+echo "docker-compose run --rm freqtrade backtesting --datadir user_data/data/binance --export trades --stake-amount 100 -i 1h --timerange=20210101- --strategy-list " > run_all_strats.sh
+echo "! Listing 'ls -1a strategies/*.py'"
+ls -1a strategies/*.py | tee all_strategies.txt
+cat all_strategies.txt | sed "s/.py//; s/strategies\///" > final_all_strategies.txt
+echo "! Cleaning up..."
+rm all_strategies.txt
+echo "! Final 'final_all_strategies.txt' list"
+cat final_all_strategies.txt
+echo "! Done"
--- user_data/run_all_strats.sh
+++ user_data/run_all_strats.sh
@@ -4,7 +4,7 @@ docker-compose run --rm freqtrade backtesting \
 --datadir user_data/data/binance \
 --export trades \
 --stake-amount 100 \
---timeframe 15m \
+--timeframe 1h \
 --timerange=20210101- \
 --strategy-list \
 ADXMomentum \
